542f316076a12d5054c7b4245661992e516b1e4f
devignesh <vinesh1865@gmail.com>
2018-08-19 17:32:38 +0530

    pollapp files

--- pollapp/urls.py
+++ pollapp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include, path
+from poll import views
+app_name = 'poll'
 urlpatterns = [
+    path('poll/', include('poll.urls')),
     path('admin/', admin.site.urls),
+    
 ]
