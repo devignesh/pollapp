a965b0347f51c86d79c68b1908719f57b0415983
devignesh <vinesh1865@gmail.com>
2018-08-19 19:26:37 +0530

    page viewing

--- pollapp/urls.py
+++ pollapp/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from poll import views
+from django.utils import timezone
 app_name = 'poll'
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('poll/', include('poll.urls')),
     path('admin/', admin.site.urls),
     
